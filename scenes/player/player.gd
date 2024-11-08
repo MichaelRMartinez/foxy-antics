04710a84db3b1ea2c8870e4afb46a9cfba61397b
Michael <mirima92@gmail.com>
2024-11-07 22:34:47 -0500

    Add player input, movement, and jump controls; clamp y velocity

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 class_name Player
 
 const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const JUMP_VELOCITY: float = -260.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +19,21 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
 	
+	get_input()
 	move_and_slide()
+
+
+func get_input() -> void:
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left") == true:
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	elif Input.is_action_pressed("right") == true:
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
+		
+	if is_on_floor() == true and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+	
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
