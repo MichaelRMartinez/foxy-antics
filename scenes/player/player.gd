d81729a51cece85fc7c2e4b446738208562baa7c
Michael <mirima92@gmail.com>
2025-02-12 00:29:19 -0500

    Add player left/right movement and jump functionality

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
+
 class_name Player
 
-const GRAVITY: float = 690.0
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const JUMP_VELOCITY: float = -260.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,4 +22,24 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
 	
+	get_input()
+	
 	move_and_slide()
+
+
+func get_input() -> void:
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left") == true:
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	elif Input.is_action_pressed("right") == true:
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+	
+	
